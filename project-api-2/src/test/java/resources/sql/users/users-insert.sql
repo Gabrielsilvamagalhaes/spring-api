caae1c485ef351c1f8a7cbd4c06536c812b1d8d2
Gabriel Silva <gabiles278@gmail.com>
2024-03-10 08:57:55 -0300

    Configuring the testing environment

---
+++ project-api-2/src/test/java/resources/sql/users/users-insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO USERS(id, username, password, role) values (100, 'arroz@gmail.com', '123456', 'ADMIN');
+INSERT INTO USERS(id, username, password, role) values (101, 'feijao@gmail.com', '123456', 'CLIENT');
+INSERT INTO USERS(id, username, password, role) values (102, 'frango@gmail.com', '123456', 'CLIENT');
