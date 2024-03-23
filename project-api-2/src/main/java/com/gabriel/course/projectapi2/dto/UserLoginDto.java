fb8d8d0e8453d6232e4c606a168713501889b770
Gabriel Silva <gabiles278@gmail.com>
2024-03-23 10:42:29 -0300

    The Authentication controller

---
+++ project-api-2/src/main/java/com/gabriel/course/projectapi2/dto/UserLoginDto.java
@@ -0,0 +1,23 @@
+package com.gabriel.course.projectapi2.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UserLoginDto {
+
+    @NotBlank
+    @Email(message = "Formato de e-mail inválido!")
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String password;
+
+}
