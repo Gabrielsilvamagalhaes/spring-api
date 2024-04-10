6d55303b2992782446acbe49a9cc9ea18dc80b8e
Gabriel Silva <gabiles278@gmail.com>
2024-04-09 21:03:03 -0300

    feat: Creating and mapping the Customer entity

---
+++ project-api-2/src/main/java/com/gabriel/course/projectapi2/model/Client.java
@@ -0,0 +1,54 @@
+package com.gabriel.course.projectapi2.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "app_clients")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@EntityListeners(AuditingEntityListener.class)
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cpf", nullable = false, unique = true, length = 11)
+    private String cpf;
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+    @OneToOne
+    @JoinColumn(name = "id_user", nullable = false)
+    private User user;
+
+    @CreatedDate
+    @Column(name="date_create")
+    private LocalDateTime dateCreate;
+    @LastModifiedDate
+    @Column(name="date_update")
+    private LocalDateTime dateUpdate;
+
+    //	Campo usado para registrar o usuário que fez o insert na tabela
+    @CreatedBy
+    @Column(name="name_create")
+    private String nameCreate;
+    //	Campo usado para registrar o usuário que fez o update na tabela
+    @LastModifiedBy
+    @Column(name="name_update")
+    private String nameUpdate;
+
+}
