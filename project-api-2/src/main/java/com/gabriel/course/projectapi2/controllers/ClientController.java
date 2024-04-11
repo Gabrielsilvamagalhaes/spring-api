7de00111a9d96ee5b3d309dc49aa7ce8cb264afc
Gabriel Silva <gabiles278@gmail.com>
2024-04-11 17:12:01 -0300

    refactor: refactoring the documentation feature for customer listing

--- project-api-2/src/main/java/com/gabriel/course/projectapi2/controllers/ClientController.java
+++ project-api-2/src/main/java/com/gabriel/course/projectapi2/controllers/ClientController.java
@@ -24,6 +24,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -69,7 +70,7 @@ public class ClientController {
             })
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<PageableDto> getAllClients(Pageable pageable) {
+    public ResponseEntity<PageableDto> getAllClients(@Parameter(hidden = true) @PageableDefault(size = 5, sort = {"name"}) Pageable pageable) {
         Page<ClientProjection> clients = clientService.findAllClients(pageable);
 
         return ResponseEntity.ok(PageableMapper.toDto(clients));
